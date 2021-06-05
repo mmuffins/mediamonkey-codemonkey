c11b90a1f2b4d9900990d0735edb3118fc5c8739
mmuffins <mail@tiv73.com>
2021-06-05 23:43:42 +0200

    Fix hard to read dynamic font colors

--- templates_add.js
+++ templates_add.js
@@ -4,22 +4,41 @@ window.templates.findLVStyleColorTypes = function () {
     tbHoverStyle = 'background';
 };
 
+//JL: Save colors in memory so that repeatedly-opened albums do not need to repeatedly calculate colors.
+var cachedColors = {};
+
 window.templates.setLVPopupStyles = function (img, div) {
     templates.findLVStyleColorTypes();
     var LV = div.parentListView;
-    var clr = colorThief.getColor(img);
+    
+    var clr;
+    var path = img.getAttribute('src');
+    if (cachedColors[path]) {
+        clr = cachedColors[path];
+        console.log(cachedColors);
+    }
+    else {
+        clr = colorThief.getColor(img);
+        cachedColors[path] = clr;
+    }
+    
     var backclr = 'rgb(' + clr[0] + ', ' + clr[1] + ', ' + clr[2] + ')';
     div.style.backgroundColor = backclr;
     LV.createPopupIndicator().style.fill = backclr;
 
-    // change background color
+	var nowPlayingClr = ''; //nowPlaying color is !important by default in skin
+	if (clr[0] > 110 && (clr[1] + clr[2]) / 2 < clr[0] * 0.75) // r > 110 and avg of g&b < 0.75*r
+		nowPlayingClr = 'rgb(214, 227, 255)!important'; // if the background color is red, set the now playing color to a contrasting color (b/c pink blends in with it)
+
+    // change background color	
     if ((clr[0] + clr[1] + clr[2]) / 3 < 128) {
         var hClr = getShiftedRGB(20, clr);
         templates.setPopupListColors(div.controlClass.uniqueID, {
-            text: 'red',
+            text: 'white',
             hover: hClr,
             select: getShiftedRGB(50, clr),
-            nowplaying: 'rgb(255, 255, 150)',
+            hoverSelect: getShiftedRGB(70, clr),
+            nowplaying: nowPlayingClr,
             iconsHover: hClr,
             textHover: 'rgb(255, 215, 70)'
         }, div.parentElement.controlClass.uniqueID);
@@ -29,7 +48,8 @@ window.templates.setLVPopupStyles = function (img, div) {
             text: 'black',
             hover: hClr,
             select: getShiftedRGB(-50, clr),
-            nowplaying: 'rgb(128, 128, 0)',
+            hoverSelect: getShiftedRGB(-70, clr),
+            nowplaying: nowPlayingClr,
             iconsHover: hClr,
             textHover: 'rgb(90, 90, 0)'
         }, div.parentElement.controlClass.uniqueID);
