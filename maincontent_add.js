c3b3ee60d028a9c43e787be2eef92bc813cc5d2e
mmuffins <mail@tiv73.com>
2022-01-19 16:21:46 +0100

    Attach progress container to document body

--- maincontent_add.js
+++ maincontent_add.js
@@ -1,1 +1,19 @@
 thisWindow.frameColor = 0xcf6d34;
+
+// Attach progress bar to bottom of window with a slight offset
+var progressContainerDiv = document.createElement('div');
+progressContainerDiv.setAttribute('data-id','progressContainer');
+progressContainerDiv.setAttribute('data-hideInFullWindowMode','');
+progressContainerDiv.classList.add("content");
+progressContainerDiv.style.position = 'absolute';
+progressContainerDiv.style.position = 'fixed';
+progressContainerDiv.style.bottom = 0;
+progressContainerDiv.style.left = "11%";
+progressContainerDiv.style.right = 0;
+document.body.appendChild(progressContainerDiv);
+
+
+//var middlePanel = document.querySelector('[data-id="viewControl_MiddlePanel"]');
+// var middlePanel = qid('viewControl_MiddlePanel');
+var middlePanel = qid('mainview');
+middlePanel.body.appendChild(progressContainerDiv);
